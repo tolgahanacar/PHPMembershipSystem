59e9c6ad0fb83f0d93d642e8248fcc6f8f5f1da5
tolgahanacar <47753083+tolgahanacar@users.noreply.github.com>
2020-09-30 16:04:36 +0300

    Update control.php
    Update control.php

--- Controls/control.php
+++ Controls/control.php
@@ -3,14 +3,16 @@ require_once '../Settings/config.php';
 ?>
 
 <?php
-$username = strip_tags(trim($_POST['username']));
-$name = strip_tags(trim($_POST['name']));
-$surname =strip_tags(trim($_POST['surname']));
-$email = strip_tags(trim($_POST['email']));
-$password = strip_tags(trim(md5($_POST['password'])));
-$repeatpassword = strip_tags(trim(md5($_POST['repeatpassword'])));
+$username = htmlspecialchars(strip_tags(trim($_POST['username'])));
+$name     = htmlspecialchars(strip_tags(trim($_POST['name'])));
+$surname  = htmlspecialchars(strip_tags(trim($_POST['surname'])));
+$email    = htmlspecialchars(strip_tags(trim($_POST['email'])));
+$password = htmlspecialchars(strip_tags(trim(md5($_POST['password']))));
+$repeatpassword = htmlspecialchars(strip_tags(trim(md5($_POST['repeatpassword']))));
 if($password==$repeatpassword){
-    $Ekle = $VeriTabaniBaglantisi->query("INSERT INTO users (Username,UName,USurname,UEMail,UPassword) VALUES ('$username','$name','$surname','$email','$password')");
+    $Ekle = $VeriTabaniBaglantisi->prepare("INSERT INTO users (Username,UName,USurname,UEMail,UPassword) VALUES ('$username','$name','$surname','$email','$password')");
+    $Ekle->execute();
+    $Say  = $Ekle->rowCount();
     if($Ekle){
         echo "Registration Successful";
         header("refresh:1; url=../login.php");
